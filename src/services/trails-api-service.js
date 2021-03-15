f04c034d0ba3ac3a280745cd463792cf795bc207
Liron de Castro <lirondecastro@Lirons-MacBook-Air.local>
2021-03-15 16:13:17 -0700

    trails api service

--- src/services/trails-api-service.js
+++ src/services/trails-api-service.js
@@ -1,5 +1,95 @@
-import React, { Component } from 'react'
+import config from '../config'
+import TokenService from './token-service'
 
-export const nullTrail = {
-    location: {}
+const TrailsApiService = {
+    getTrails(query) {
+        return fetch(`${config.API_ENDPOINT}/trails?region=${query.region}&name=${query.name}&rating=${query.rating}&difficulty=${query.difficulty}`, {
+            headers: {
+                'authorization': `bearer ${TokenService.getAuthToken()}`
+            },
+        })
+            .then(res => {
+                (!res.ok)
+                    ? res.json().then(e => Promise.reject(e))
+                    : res.json()
+            })
+    },
+
+    getTrail(trailId) {
+        return fetch(`${config.API_ENDPOINT}/trails/${trailId}`, {
+            headers: {
+                'authorization': `bearer ${TokenService.getAuthToken}`
+            },
+        })
+            .then(res => {
+                (!res.ok)
+                    ? res.json().then(e => Promise.reject(e))
+                    : res.json()
+            })
+    },
+
+    getTraiLComments(trailId) {
+        return fetch(`${config.API_ENDPOINT}/trails/${trailId}/comments`, {
+            headers: {
+                'authorization': `bearer ${TokenService.getAuthToken}`
+            },
+        })
+            .then(res => {
+                (!res.ok)
+                    ? res.json().then(e => Promise.reject(e))
+                    : res.json()
+            })
+    },
+
+    getTrailRatings(trailId) {
+        return fetch(`${config.API_ENDPOINT}/trails/${trailId}/ratings`, {
+            headers: {
+                'authorization': `bearer ${TokenService.getAuthToken}`
+            },
+        })
+            .then(res => {
+                (!res.ok)
+                    ? res.json().then(e => Promise.reject(e))
+                    : res.json()
+            })
+    },
+
+    postComment(trailId, content) {
+        return fetch(`${config.API_ENDPOINT}/comments/${trailId}`, {
+            method: 'POST',
+            headers: {
+                'content-type': ' application/json',
+                'authorization': `bearer ${TokenService.getAuthToken}`
+            },
+            body: JSON.stringify({
+                content
+            })
+        })
+            .then(res => {
+                (!res.ok)
+                    ? res.json().then(e => Promise.reject(e))
+                    : res.json()
+            })
+    },
+
+    postRating(trailId, rating) {
+        return fetch(`${config.API_ENDPOINT}/ratings/${trailId}`, {
+            method: 'POST',
+            headers: {
+                'content-type': ' application/json',
+                'authorization': `bearer ${TokenService.getAuthToken}`
+            },
+            body: JSON.stringify({
+                rating
+            })
+        })
+            .then(res => {
+                (!res.ok)
+                    ? res.json().then(e => Promise.reject(e))
+                    : res.json()
+            })
+    },
+
 }
+
+export default TrailsApiService
